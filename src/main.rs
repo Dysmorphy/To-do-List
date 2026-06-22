a605403eaaeb69c7f59ca9732125e3cafdadae94
Nebel <georgiyzharichev@gmail.com>
2026-06-22 22:56:21 +0300

    Implemented a Result return value for delete_task method to handle missing task id

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #[derive(Debug)]
+#[derive(Clone)]
 struct Task {
     desc:String,
     id:u32,
@@ -39,20 +40,23 @@ impl TaskManager {
         }
     }
 
-    fn delete_task(&mut self,search_id:u32) {
-        let mut found = false;
+    fn delete_task(&mut self,search_id:u32) -> Result<Task,String> {
+        let mut found_idx:Option<usize> = None;
         for (task_idx,task) in self.tasks.iter().enumerate() {
             if task.id == search_id {
-                self.tasks.remove(task_idx);
-                found = true;
+                found_idx = Some(task_idx);
                 break;
             }
         }
 
-        if found == false {
-         //figure out error messaging   
+        match found_idx {
+            Some(delete_idx) => {
+                let removed_task = self.tasks.remove(delete_idx);
+                self.fix_ids();
+                Ok(removed_task)
+            }
+            None => Err("id not found".to_string())
         }
-        self.fix_ids();
     }
     
 }
